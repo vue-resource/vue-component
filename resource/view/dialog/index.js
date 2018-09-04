23ed5313d6f8028f1e3c9da96159f370a84bc266
liguang <liguang@idstaff.com>
2018-09-04 11:53:24 +0800

    完成了confirm插件的开发

--- resource/view/dialog/index.js
+++ resource/view/dialog/index.js
@@ -154,14 +154,15 @@ var tooltip_vue = {
  *     title: 确认框标题，默认："提示"
  *     lock: 弹框弹起时，是否锁定页面滚动，默认：true
  *     width:确认框尺寸，默认40%
- *     showTitle:是否展示头部区域，默认：true
+ *     showHead:是否展示头部区域，默认：true
  *     showClose:是否显示关闭按钮，默认:true
  *     content:弹框内容，默认:""
  *     dangerouslyUseHTMLString:是否解析html字符串，默认：false
  *     align:底部按钮的对齐方式，有left,center,right3种，默认:center
+ *     onlySure:是否只展示确认按钮,默认false
+ *     btns:按钮文案，默认：["取消","确认"]
  *     slot:插槽处理
  *       tool:头部工具插槽
- *       foot：底部按钮区域插槽
  *     okFn: 成功回调函数
  *     failFn:失败回调函数
  */
@@ -169,13 +170,16 @@ var confirm_vue = {
   template:'<div class="mask" v-if="visible">'
             +'<div class="mask-bg" @click="visible=false"></div>'
             +'<div class="confirm" ref="confirm">'
-              +'<div class="confirm-head" v-if="baseOpt.showTitle">'
+              +'<div class="confirm-head" v-if="baseOpt.showHead">'
                 +'<span class="confirm-title">{{baseOpt.title}}</span>'
-                +'<slot name="tool"></slot>'
-                +'<span class="icon icon-delete confirm-close" v-if="baseOpt.showClose"></span>'
+                +'<span class="confirm-tool" v-if="baseOpt.slot.tool.length>0"></span>'
+                +'<span class="icon icon-delete confirm-close" v-if="baseOpt.showClose" @click="visible=false"></span>'
               +'</div>'
               +'<div class="confirm-content"></div>'
-              +'<div class="confirm-foot"><slot name="foot"></slot></div>'
+              +'<div class="confirm-foot">'
+                +'<span class="btn" @click="baseOpt.failFn" v-if="!baseOpt.onlySure">{{baseOpt.btns[0]}}</span>'
+                +'<span class="btn btn-blue" @click="baseOpt.okFn">{{baseOpt.btns[1]}}</span>'
+              +'</div>'
             +'</div></div>',
   data:function(){
     var self = this;
@@ -183,20 +187,30 @@ var confirm_vue = {
       baseOpt:{
         title:"提示",
         lock:true,
-        width:'40%',
-        showTitle:true,
+        width:'30%',
+        showHead:true,
         showClose:true,
         content:"",
         dangerouslyUseHTMLString:false,
         align:"center",
+        onlySure:false,
+        btns:["取消","确认"],
         slot:{
-          tool:"",
-          content:'<span class="btn" @click="failFn">取消</span><span class="btn btn-warning" @click="okFn">确定</span>'
+          tool:""
         },
         okFn:function(){self.visible = false},
         failFn:function(){self.visible = false}
       },
       visible:false
+    }
+  },
+  watch:{
+    'visible':function(nv,ov){
+      if(nv && this.baseOpt.lock){//设置锁定并且弹框已经出现
+        document.body.classList.add('fixed-full')
+      }else{
+        document.body.classList.remove('fixed-full')
+      }
     }
   },
   methods:{
@@ -209,7 +223,16 @@ var confirm_vue = {
     })
    },
    renderConfirm:function(){
-    
+    //渲染尺寸
+    this.$refs.confirm.style.width = this.baseOpt.width;
+    //渲染内容主体
+    this.$refs.confirm.querySelector('.confirm-content')[this.baseOpt.dangerouslyUseHTMLString?'innerHTML':'textContent'] = this.baseOpt.content
+    //底部按钮布局
+    this.$refs.confirm.querySelector('.confirm-foot').classList.add("align-"+this.baseOpt.align)
+    //渲染工具栏
+    if(this.baseOpt.slot.tool.length>0){
+      this.$refs.confirm.querySelector('.confirm-tool').innerHTML = this.baseOpt.slot.tool
+    }
    }
   }
 }
